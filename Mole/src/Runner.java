f9cce94670b338d1815f4fd27a726eeb840bac35
MichaelMvv <mikey.tnt.mc@gmail.com>
2015-01-01 22:22:38 -0800

    fix stuffs

--- Mole/src/Runner.java
+++ Mole/src/Runner.java
@@ -4,28 +4,31 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class Runner implements ActionListener {
 	Game game = new Game();
 	static JFrame frame = new JFrame();
 	static JPanel panel = new JPanel();
 
-
 	public static void main(String[] args) {
-new Runner().start();	
+		new Runner().start();
 
 	}
-public  void start() {
-	for (int i = 0; i < 3; i++) {
-		JButton b = new JButton();
-		b.addActionListener(this);
-		b.setText(i + "");
-		panel.add(b);
+
+	public void start() {
+		panel.add(new JTextArea("How hard?"));
+		for (int i = 0; i < 3; i++) {
+			JButton b = new JButton();
+			b.addActionListener(this);
+			b.setText(i + "");
+			panel.add(b);
+		}
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
 	}
-	frame.add(panel);
-	frame.pack();
-	frame.setVisible(true);
-}
+
 	public void actionPerformed(ActionEvent arg0) {
 		JButton buttonPressed = (JButton) arg0.getSource();
 		if (buttonPressed.getText().equalsIgnoreCase("0")) {
